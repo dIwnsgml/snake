3fdbec16dacb971bb936e9690861ed75581f1612
dlwnsgmI <junjason1126@gmail.com>
2022-09-28 04:29:54 -0700

    eff f

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fileinput import filename
+from winreg import DisableReflectionKey
 from PIL import ImageGrab, Image
 import cv2
 import numpy as np
@@ -15,6 +16,8 @@ root = Tk()
 root.title("snake")
 root.geometry("500x500")
 
+global direction
+direction = 0
 def search_apple(x, y):
   with mss() as sct:
     sct.shot()
@@ -27,28 +30,94 @@ def search_apple(x, y):
   if(len(cor[0])!=0):
     cor = cor[1][0], cor[0][0]
   else:
-    cor = 0, 0
+    cor = 1000
   return cor
 
+
+def change_y(pre_y, cur_y, dir):
+  global direction
+  if(dir == 0):
+    print("dir = 0")
+    if(pre_y < cur_y):
+      #turn right(bottom)
+      print("down")
+      keyboard.press_and_release('down arrow')
+      time.sleep((cur_y - pre_y) / 5.5)
+      direction = 1
+    elif(pre_y > cur_y):
+      #turn left(up)
+      print("up")
+      keyboard.press_and_release('up arrow')
+      time.sleep((pre_y - cur_y) / 5.5)
+      direction = 3
+
+  if(dir == 2):
+    if(pre_y < cur_y):
+      #turn right(bottom)
+      keyboard.press_and_release('up')
+      time.sleep((cur_y - pre_y) / 5.5)
+      direction = 3
+    elif(pre_y > cur_y):
+      #turn left(up)
+      keyboard.press_and_release('down')
+      time.sleep((pre_y - cur_y) / 5.5)
+      direction = 1
+
+def change_x(pre_x, cur_x, dir):
+  global direction
+  print("dsdsdsd")
+  if(dir == 1):
+    print("dir = 1")
+    if(pre_x < cur_x):
+      #turn right(bottom)
+      print("down")
+      keyboard.press_and_release('right')
+      time.sleep((cur_x - pre_x) / 5.5)
+      direction = 0
+    elif(pre_x > cur_x):
+      #turn left(up)
+      print("up")
+      keyboard.press_and_release('left')
+      time.sleep((pre_x - cur_x) / 5.5)
+      direction = 3
+
+  if(dir == 3):
+    print('dir = 3')
+    if(pre_x < cur_x):
+      #turn right(bottom)
+      keyboard.press_and_release('left')
+      time.sleep((cur_x - pre_x) / 5.5)
+      direction = 1
+    elif(pre_x > cur_x):
+      #turn left(up)
+      keyboard.press_and_release('right')
+      time.sleep((pre_x - cur_x) / 5.5)
+      direction = 0
+      
 def start():
   global direction
-  direction = 0
   board = loc[1][0], loc[0][0]
   mouse.move(board[0], board[1])
   mouse.click('left')
   keyboard.press_and_release('right')
-  pre = [0, 0]
+  pre = [1, 1]
   for i in range(100):
     start = time.time()
     cor = search_apple(board[0], board[1])
-    print(f"{time.time() - start:.4f}")
+    if(cor == 1000):
+      cor = pc
+    #dprint(f"{time.time() - start:.4f}", cor)
     trans_cor = round(cor[0] / 67), round(cor[1] / 66)
     if((trans_cor[0] != pre[0] or trans_cor[1] != pre[1]) and i != 0):
-      #print("changed", cor[1], cor[0], trans_cor[0], trans_cor[1], pre)
+      #print("changed", pre, trans_cor)
       change_y(pre[1], trans_cor[1], direction)
-    #print(cor[1], cor[0], trans_cor[0], trans_cor[1], pre)
+      change_x(pre[0], trans_cor[0], direction)
+      print(direction)
+    print(trans_cor, pre)
+    time.sleep(0.1)
     #mouse.move(cor[0] + board[0], cor[1] + board[1])
     pre = trans_cor
+    pc = cor
 
 
   '''for i in range(3):
@@ -72,58 +141,6 @@ def search():
     loc = np.where(accuracy > 0.9)
     print(loc[1][0], loc[0][0], user_screen.shape)
     
-def change_y(pre_y, cur_y, dir):
-  if(dir == 0):
-    print("dir = 0")
-    if(pre_y < cur_y):
-      #turn right(bottom)
-      print("down")
-      keyboard.press_and_release('down arrow')
-      time.sleep((cur_y - pre_y) / 5.67)
-      keyboard.press_and_release('left')
-      direction = 1
-      return 0
-    elif(pre_y > cur_y):
-      #turn left(up)
-      print("up")
-      keyboard.press_and_release('up arrow')
-      time.sleep((pre_y - cur_y) / 5.67)
-      keyboard.press_and_release('left')
-      direction = 3
-      return 0
-
-  if(dir == 2):
-    if(pre_y < cur_y):
-      #turn right(bottom)
-      keyboard.press_and_release('up')
-      direction = 3
-      return 0
-    elif(pre_y > cur_y):
-      #turn left(up)
-      keyboard.press_and_release('down')
-      direction = 1
-      return 0
-
-def change_x(pre_x, cur_x, dir):
-  if(dir == 1):
-    if(pre_x < cur_x):
-      #turn left(right)
-      keyboard.press_and_release('right')
-      return 0
-    elif(pre_x > cur_x):
-      #turn right(left)
-      keyboard.press_and_release('left')
-      return 0
-  elif(dir == 3):
-    if(pre_x < cur_x):
-      #turn right(right)
-      keyboard.press_and_release('left')
-      return 0
-    elif(pre_x > cur_x):
-      #turn left(left)
-      keyboard.press_and_release('right')
-      return 0
-
 
 
 btn_search = Button(root, text="search", command = search)
